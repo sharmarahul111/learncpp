c59df2bc3b69409c5593cb74a40b8548d3293e6d
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-01-12 23:01:08 +0545

    multiple input

--- 01-basics/04-input.cpp
+++ 01-basics/04-input.cpp
@@ -1,8 +1,13 @@
 #include<iostream>
 int main(){
   int x {};
+  int y {};
   std::cout << "Enter a number: ";
   std::cin >> x;
   std::cout << "You entered: " << x << '\n';
+  std::cout << "Enter a number: ";
+  std::cin >> x >> y;
+  std::cout << "You entered: " << x << y << '\n';
+
   return 0;
 }
