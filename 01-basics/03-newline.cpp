985490ab2f29e35775ce63a1c590c27b411ce694
Pramendra Sharma ( Rahul ) <74184362+sharmarahul111@users.noreply.github.com>
2026-01-13 16:35:33 +0545

    Silly typo 🤦🏻‍♂️

--- 01-basics/03-newline.cpp
+++ 01-basics/03-newline.cpp
@@ -4,7 +4,7 @@ int main(){
   std::cout << "Hi" << 5 << '\n';
   // std::endl flushes the buffer
   // whuch is slow
-  // so just use \n and let C+++
+  // so just use \n and let C++
   // decide when to output after a period
   // this is efficient, since multiple outputs
   // can be batched together
