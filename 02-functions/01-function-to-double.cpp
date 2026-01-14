f6c4b89a18e1007fafd89b371d8a0670904beba0
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-01-14 10:29:29 +0545

    2.4 Quiz program

---
+++ 02-functions/01-function-to-double.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int doubleNumber(int x){
+  return x*2;
+}
+int main(){
+  std::cout << "Enter a number: ";
+  int x {};
+  std::cin >> x;
+  std::cout << "\nThe double is:" << doubleNumber(x) << '\n';
+  return 0;
+}
