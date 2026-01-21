2ebdce117096d136bb7a2f3444ad9cd1877583f0
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-01-21 15:41:54 +0545

    sizeof()

---
+++ 04-fundamentals/sizeof.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main(){
+  std::cout << "Size of: int = " << sizeof(int) << '\n';
+  std::cout << "Size of: short = " << sizeof(short) << '\n';
+  std::cout << "Size of: long = " << sizeof(long) << '\n';
+  std::cout << "Size of: char = " << sizeof(char) << '\n';
+  std::cout << "Size of: bool = " << sizeof(bool) << '\n';
+  return 0;
+}
