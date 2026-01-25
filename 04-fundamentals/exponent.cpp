4dbf1e58430bfc1902311cc98f963b74c2206cc5
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-01-25 22:30:10 +0545

    exponents

---
+++ 04-fundamentals/exponent.cpp
@@ -0,0 +1,9 @@
+#include<iostream>
+int main(){
+  float a {1.24e3};
+  std::cout << a << '\n';
+  std::cout << 1.2e5 << '\n';
+  std::cout << 1.2e-4 << '\n';
+  std::cout << 1.223e-2 << '\n';
+  return 0;
+}
