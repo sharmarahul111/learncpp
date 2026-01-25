49754e40ec28a694c81018598f5f18566da50dbc
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-01-25 22:42:35 +0545

    basic float, double and long

---
+++ 04-fundamentals/float.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+int main(){
+  // by default is double
+  // use f to denote float
+  float x {0.2f};
+  double y {0.55e2};
+  long double z {6e-10};
+  std::cout << x << '\n';
+  std::cout << y << '\n';
+  std::cout << z << '\n';
+}
