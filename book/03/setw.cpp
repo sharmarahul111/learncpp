88a33e0d1b2f24111b34c38877c99555dc15f9ac
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-04-28 16:43:00 +0545

    setw()

---
+++ book/03/setw.cpp
@@ -0,0 +1,11 @@
+#include <iomanip>
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << setw(10) << "Price:" << setw(6) << 100 << endl;
+	cout << setw(10) << "Age:" << setw(6) << 18 << endl;
+
+	return 0;
+}
