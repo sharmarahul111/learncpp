fc1494db1d5d8666ef6c267589f8903e97c44c8d
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-04-30 20:02:01 +0545

    Private inheritance

---
+++ book/08-inheritance/01-private_inheritance.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+class A
+{
+	private:
+		int a;
+	public:
+		int b;
+		int get_a()
+		{
+			return a;
+		}
+};
+class B:A
+{
+	private:
+		int c;
+	public:
+		int d;
+		void disp()
+		{
+			// cout << a << endl; // Not accessible directly
+			cout << get_a() << endl;
+			cout << b << endl;
+			cout << c << endl;
+			cout << d << endl;
+		}
+};
+
+int main()
+{
+	B b;
+	b.disp();
+	// cout << b.a << endl; // Private
+	// cout << b.b << endl; // Privately inherited so illegal
+	// cout << b.c << endl; // Private
+	cout << b.d << endl;
+	return 0;
+}
