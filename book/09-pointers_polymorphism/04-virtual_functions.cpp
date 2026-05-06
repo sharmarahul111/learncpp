14c2a376588d5076abf63f926593b72b86b985b9
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-05-06 21:43:59 +0545

    caution before using virtual methods

--- book/09-pointers_polymorphism/04-virtual_functions.cpp
+++ book/09-pointers_polymorphism/04-virtual_functions.cpp
@@ -24,6 +24,17 @@ class A
 
 int main()
 {
+	/*
+	Caution
+	- avoid default arguments in virtual functions,
+	> static binding causes base class default arguments
+	to be used instead of from derived one
+
+	- never call a virtual function from a constructor or destructor
+	> we will get static binding because the code will never to a more derived
+	class than currently executing constructor or destructor
+	> if needed, make a separate function and call it manually
+	*/
 	A *ptr;
 	A a(10);
 	B b(40, 50);
